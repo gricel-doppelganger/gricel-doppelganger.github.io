50c4371bceed1845e564e5dcb209641950dff56a
Gricel <gr3c2l@gmail.com>
2023-09-25 19:43:29 -0300

    My clone CV

---
+++ js/scripts.js
@@ -0,0 +1,353 @@
+$(document).ready(function(){
+    $(".imagen1").mouseover(function(){
+        $(".wrapper-proyecto-1").fadeIn("fast");
+        $("#id-proyectos-1").fadeIn("fast");
+    });
+});
+
+$(document).ready(function(){
+    $("#id-proyectos-1").mouseleave(function(){
+        $(".wrapper-proyecto-1").fadeOut("fast");
+        $("#id-proyectos-1").fadeOut("fast");
+    });
+});
+
+$(document).ready(function(){
+    $(".imagen2").mouseover(function(){
+        $(".wrapper-proyecto-2").fadeIn("fast");
+        $("#id-proyectos-2").fadeIn("fast");
+    });
+});
+
+$(document).ready(function(){
+    $("#id-proyectos-2").mouseleave(function(){
+        $(".wrapper-proyecto-2").fadeOut("fast");
+        $("#id-proyectos-2").fadeOut("fast");
+    });
+});
+
+$(document).ready(function(){
+    $(".imagen3").mouseover(function(){
+        $(".wrapper-proyecto-3").fadeIn("fast");
+        $("#id-proyectos-3").fadeIn("fast");
+    });
+});
+
+$(document).ready(function(){
+    $("#id-proyectos-3").mouseleave(function(){
+        $(".wrapper-proyecto-3").fadeOut("fast");
+        $("#id-proyectos-3").fadeOut("fast");
+    });
+});
+
+$(document).ready(function(){
+    $(".imagen4").mouseover(function(){
+        $(".wrapper-interfaces-1").fadeIn("fast");
+        $("#id-interfaces-1").fadeIn("fast");
+    });
+});
+
+$(document).ready(function(){
+    $("#id-interfaces-1").mouseleave(function(){
+        $(".wrapper-interfaces-1").fadeOut("fast");
+        $("#id-interfaces-1").fadeOut("fast");
+    });
+});
+
+$(document).ready(function(){
+    $(".imagen5").mouseover(function(){
+        $(".wrapper-interfaces-2").fadeIn("fast");
+        $("#id-interfaces-2").fadeIn("fast");
+    });
+});
+
+$(document).ready(function(){
+    $("#id-interfaces-2").mouseleave(function(){
+        $(".wrapper-interfaces-2").fadeOut("fast");
+        $("#id-interfaces-2").fadeOut("fast");
+    });
+});
+
+$(document).ready(function(){
+    $(".imagen6").mouseover(function(){
+        $(".wrapper-interfaces-3").fadeIn("fast");
+        $("#id-interfaces-3").fadeIn("fast");
+    });
+});
+
+$(document).ready(function(){
+    $("#id-interfaces-3").mouseleave(function(){
+        $(".wrapper-interfaces-3").fadeOut("fast");
+        $("#id-interfaces-3").fadeOut("fast");
+    });
+});
+
+$(document).ready(function(){
+    $(".imagen7").mouseover(function(){
+        $(".wrapper-ilustracion-1").fadeIn("fast");
+        $("#id-ilustracion-1").fadeIn("fast");
+    });
+});
+
+$(document).ready(function(){
+    $("#id-ilustracion-1").mouseleave(function(){
+        $(".wrapper-ilustracion-1").fadeOut("fast");
+        $("#id-ilustracion-1").fadeOut("fast");
+    });
+});
+
+$(document).ready(function(){
+    $(".imagen8").mouseover(function(){
+        $(".wrapper-ilustracion-2").fadeIn("fast");
+        $("#id-ilustracion-2").fadeIn("fast");
+    });
+});
+
+$(document).ready(function(){
+    $("#id-ilustracion-2").mouseleave(function(){
+        $(".wrapper-ilustracion-2").fadeOut("fast");
+        $("#id-ilustracion-2").fadeOut("fast");
+    });
+});
+
+$(document).ready(function(){
+    $(".imagen9").mouseover(function(){
+        $(".wrapper-ilustracion-3").fadeIn("fast");
+        $("#id-ilustracion-3").fadeIn("fast");
+    });
+});
+
+$(document).ready(function(){
+    $("#id-ilustracion-3").mouseleave(function(){
+        $(".wrapper-ilustracion-3").fadeOut("fast");
+        $("#id-ilustracion-3").fadeOut("fast");
+    });
+});
+
+// EN MÓVILES //
+
+$(document).ready(function(){
+    $(".imagen1").tap(function(){
+        $(".wrapper-proyecto-1").fadeIn("fast");
+        $("#id-proyectos-1").fadeIn("fast");
+        $(".wrapper-proyecto-2").fadeOut("fast");
+        $("#id-proyectos-2").fadeOut("fast");
+        $(".wrapper-proyecto-3").fadeOut("fast");
+        $("#id-proyectos-3").fadeOut("fast");
+        $(".wrapper-interfaces-1").fadeOut("fast");
+        $("#id-interfaces-1").fadeOut("fast");
+        $(".wrapper-interfaces-2").fadeOut("fast");
+        $("#id-interfaces-2").fadeOut("fast");
+        $(".wrapper-interfaces-3").fadeOut("fast");
+        $("#id-interfaces-3").fadeOut("fast");
+        $(".wrapper-ilustracion-1").fadeOut("fast");
+        $("#id-ilustracion-1").fadeOut("fast");
+        $(".wrapper-ilustracion-2").fadeOut("fast");
+        $("#id-ilustracion-2").fadeOut("fast");
+        $(".wrapper-ilustracion-3").fadeOut("fast");
+        $("#id-ilustracion-3").fadeOut("fast");
+    });
+});
+
+$(document).ready(function(){
+    $(".imagen2").tap(function(){
+        $(".wrapper-proyecto-2").fadeIn("fast");
+        $("#id-proyectos-2").fadeIn("fast");
+        $(".wrapper-proyecto-1").fadeOut("fast");
+        $("#id-proyectos-1").fadeOut("fast");
+        $(".wrapper-proyecto-3").fadeOut("fast");
+        $("#id-proyectos-3").fadeOut("fast");
+        $(".wrapper-interfaces-1").fadeOut("fast");
+        $("#id-interfaces-1").fadeOut("fast");
+        $(".wrapper-interfaces-2").fadeOut("fast");
+        $("#id-interfaces-2").fadeOut("fast");
+        $(".wrapper-interfaces-3").fadeOut("fast");
+        $("#id-interfaces-3").fadeOut("fast");
+        $(".wrapper-ilustracion-1").fadeOut("fast");
+        $("#id-ilustracion-1").fadeOut("fast");
+        $(".wrapper-ilustracion-2").fadeOut("fast");
+        $("#id-ilustracion-2").fadeOut("fast");
+        $(".wrapper-ilustracion-3").fadeOut("fast");
+        $("#id-ilustracion-3").fadeOut("fast");
+    });
+});
+
+
+$(document).ready(function(){
+    $(".imagen3").tap(function(){
+        $(".wrapper-proyecto-3").fadeIn("fast");
+        $("#id-proyectos-3").fadeIn("fast");
+        $(".wrapper-proyecto-1").fadeOut("fast");
+        $("#id-proyectos-1").fadeOut("fast");
+        $(".wrapper-proyecto-2").fadeOut("fast");
+        $("#id-proyectos-2").fadeOut("fast");
+        $(".wrapper-interfaces-1").fadeOut("fast");
+        $("#id-interfaces-1").fadeOut("fast");
+        $(".wrapper-interfaces-2").fadeOut("fast");
+        $("#id-interfaces-2").fadeOut("fast");
+        $(".wrapper-interfaces-3").fadeOut("fast");
+        $("#id-interfaces-3").fadeOut("fast");
+        $(".wrapper-ilustracion-1").fadeOut("fast");
+        $("#id-ilustracion-1").fadeOut("fast");
+        $(".wrapper-ilustracion-2").fadeOut("fast");
+        $("#id-ilustracion-2").fadeOut("fast");
+        $(".wrapper-ilustracion-3").fadeOut("fast");
+        $("#id-ilustracion-3").fadeOut("fast");
+    });
+});
+
+$(document).ready(function(){
+    $(".imagen4").tap(function(){
+        $(".wrapper-interfaces-1").fadeIn("fast");
+        $("#id-interfaces-1").fadeIn("fast");
+        $(".wrapper-proyecto-1").fadeOut("fast");
+        $("#id-proyectos-1").fadeOut("fast");
+        $(".wrapper-proyecto-2").fadeOut("fast");
+        $("#id-proyectos-2").fadeOut("fast");
+        $(".wrapper-proyecto-3").fadeOut("fast");
+        $("#id-proyectos-3").fadeOut("fast");
+        $(".wrapper-interfaces-2").fadeOut("fast");
+        $("#id-interfaces-2").fadeOut("fast");
+        $(".wrapper-interfaces-3").fadeOut("fast");
+        $("#id-interfaces-3").fadeOut("fast");
+        $(".wrapper-ilustracion-1").fadeOut("fast");
+        $("#id-ilustracion-1").fadeOut("fast");
+        $(".wrapper-ilustracion-2").fadeOut("fast");
+        $("#id-ilustracion-2").fadeOut("fast");
+        $(".wrapper-ilustracion-3").fadeOut("fast");
+        $("#id-ilustracion-3").fadeOut("fast");
+    });
+});
+
+$(document).ready(function(){
+    $(".imagen5").tap(function(){
+        $(".wrapper-interfaces-2").fadeIn("fast");
+        $("#id-interfaces-2").fadeIn("fast");
+        $(".wrapper-proyecto-1").fadeOut("fast");
+        $("#id-proyectos-1").fadeOut("fast");
+        $(".wrapper-proyecto-2").fadeOut("fast");
+        $("#id-proyectos-2").fadeOut("fast");
+        $(".wrapper-proyecto-3").fadeOut("fast");
+        $("#id-proyectos-3").fadeOut("fast");
+        $(".wrapper-interfaces-1").fadeOut("fast");
+        $("#id-interfaces-1").fadeOut("fast");
+        $(".wrapper-interfaces-3").fadeOut("fast");
+        $("#id-interfaces-3").fadeOut("fast");
+        $(".wrapper-ilustracion-1").fadeOut("fast");
+        $("#id-ilustracion-1").fadeOut("fast");
+        $(".wrapper-ilustracion-2").fadeOut("fast");
+        $("#id-ilustracion-2").fadeOut("fast");
+        $(".wrapper-ilustracion-3").fadeOut("fast");
+        $("#id-ilustracion-3").fadeOut("fast");
+    });
+});
+
+$(document).ready(function(){
+    $(".imagen6").tap(function(){
+        $(".wrapper-interfaces-3").fadeIn("fast");
+        $("#id-interfaces-3").fadeIn("fast");
+        $(".wrapper-proyecto-1").fadeOut("fast");
+        $("#id-proyectos-1").fadeOut("fast");
+        $(".wrapper-proyecto-2").fadeOut("fast");
+        $("#id-proyectos-2").fadeOut("fast");
+        $(".wrapper-proyecto-3").fadeOut("fast");
+        $("#id-proyectos-3").fadeOut("fast");
+        $(".wrapper-interfaces-1").fadeOut("fast");
+        $("#id-interfaces-1").fadeOut("fast");
+        $(".wrapper-interfaces-2").fadeOut("fast");
+        $("#id-interfaces-2").fadeOut("fast");
+        $(".wrapper-ilustracion-1").fadeOut("fast");
+        $("#id-ilustracion-1").fadeOut("fast");
+        $(".wrapper-ilustracion-2").fadeOut("fast");
+        $("#id-ilustracion-2").fadeOut("fast");
+        $(".wrapper-ilustracion-3").fadeOut("fast");
+        $("#id-ilustracion-3").fadeOut("fast");
+    });
+});
+
+$(document).ready(function(){
+    $(".imagen7").tap(function(){
+        $(".wrapper-ilustracion-1").fadeIn("fast");
+        $("#id-ilustracion-1").fadeIn("fast");
+        $(".wrapper-proyecto-1").fadeOut("fast");
+        $("#id-proyectos-1").fadeOut("fast");
+        $(".wrapper-proyecto-2").fadeOut("fast");
+        $("#id-proyectos-2").fadeOut("fast");
+        $(".wrapper-proyecto-3").fadeOut("fast");
+        $("#id-proyectos-3").fadeOut("fast");
+        $(".wrapper-interfaces-1").fadeOut("fast");
+        $("#id-interfaces-1").fadeOut("fast");
+        $(".wrapper-interfaces-2").fadeOut("fast");
+        $("#id-interfaces-2").fadeOut("fast");
+        $(".wrapper-interfaces-3").fadeOut("fast");
+        $("#id-interfaces-3").fadeOut("fast");
+        $(".wrapper-ilustracion-2").fadeOut("fast");
+        $("#id-ilustracion-2").fadeOut("fast");
+        $(".wrapper-ilustracion-3").fadeOut("fast");
+        $("#id-ilustracion-3").fadeOut("fast");
+    });
+});
+
+$(document).ready(function(){
+    $(".imagen8").tap(function(){
+        $(".wrapper-ilustracion-2").fadeIn("fast");
+        $("#id-ilustracion-2").fadeIn("fast");
+        $(".wrapper-proyecto-1").fadeOut("fast");
+        $("#id-proyectos-1").fadeOut("fast");
+        $(".wrapper-proyecto-2").fadeOut("fast");
+        $("#id-proyectos-2").fadeOut("fast");
+        $(".wrapper-proyecto-3").fadeOut("fast");
+        $("#id-proyectos-3").fadeOut("fast");
+        $(".wrapper-interfaces-1").fadeOut("fast");
+        $("#id-interfaces-1").fadeOut("fast");
+        $(".wrapper-interfaces-2").fadeOut("fast");
+        $("#id-interfaces-2").fadeOut("fast");
+        $(".wrapper-interfaces-3").fadeOut("fast");
+        $("#id-interfaces-3").fadeOut("fast");
+        $(".wrapper-ilustracion-1").fadeOut("fast");
+        $("#id-ilustracion-1").fadeOut("fast");
+        $(".wrapper-ilustracion-3").fadeOut("fast");
+        $("#id-ilustracion-3").fadeOut("fast");
+    });
+});
+
+$(document).ready(function(){
+    $(".imagen9").tap(function(){
+        $(".wrapper-ilustracion-3").fadeIn("fast");
+        $("#id-ilustracion-3").fadeIn("fast");
+        $(".wrapper-proyecto-1").fadeOut("fast");
+        $("#id-proyectos-1").fadeOut("fast");
+        $(".wrapper-proyecto-2").fadeOut("fast");
+        $("#id-proyectos-2").fadeOut("fast");
+        $(".wrapper-proyecto-3").fadeOut("fast");
+        $("#id-proyectos-3").fadeOut("fast");
+        $(".wrapper-interfaces-1").fadeOut("fast");
+        $("#id-interfaces-1").fadeOut("fast");
+        $(".wrapper-interfaces-2").fadeOut("fast");
+        $("#id-interfaces-2").fadeOut("fast");
+        $(".wrapper-interfaces-3").fadeOut("fast");
+        $("#id-interfaces-3").fadeOut("fast");
+        $(".wrapper-ilustracion-1").fadeOut("fast");
+        $("#id-ilustracion-1").fadeOut("fast");
+        $(".wrapper-ilustracion-2").fadeOut("fast");
+        $("#id-ilustracion-2").fadeOut("fast");
+    });
+});
+
+
+function scrollNav() {
+  $('.nav a').click(function(){  
+    //Toggle Class
+    $(".active").removeClass("active");      
+    $(this).closest('li').addClass("active");
+    var theClass = $(this).attr("class");
+    $('.'+theClass).parent('li').addClass('active');
+    //Animate
+    $('html, body').stop().animate({
+        scrollTop: $( $(this).attr('href') ).offset().top - 160
+    }, 400);
+    return false;
+  });
+  $('.scrollTop a').scrollTop();
+}
+scrollNav();
